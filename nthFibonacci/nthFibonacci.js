e7533c95254ad4076a323618bda1e0380a426460
nadaa <nada.saif7@gmail.com>
2018-07-24 12:44:17 +0300

    nfab

--- nthFibonacci/nthFibonacci.js
+++ nthFibonacci/nthFibonacci.js
@@ -24,6 +24,13 @@ DO NOT use a recursive solution to this problem. Your solution must run in linea
 */
 
 
-nthFibonacci = function(n) {  
- // your code here
+nthFibonacci = function(n) { 
+    var f=[];
+    f[0]=1;
+    f[1]=1;
+    for(var i=2;i<=n;i++){
+        f[i]=f[i-1]+f[i-2];
+    }
+  return f[n];
 };
+nthFibonacci(4)
